5588df1e1c3c0a75bcc13072c89bc023d8b151db
issamBoutissante <boutissante.issam.dev@gmail.com>
2021-01-04 11:24:48 +0100

    la creation de base de donnees et termine

--- bibliotheque.sql
+++ bibliotheque.sql
@@ -1,46 +1,59 @@
-create database bibliotheque
+﻿create database bibliotheque
+on primary(
+name=bibliotheque_data,
+filename='C:\Users\bouti\OneDrive\سطح المكتب\Base De donne\Tp\Tp1\Biblio\bibliotheque.mdf',
+size=10Mb
+)
+log on(
+name=bibliotheque_log,
+filename='C:\Users\bouti\OneDrive\سطح المكتب\Base De donne\Tp\Tp1\Biblio\bibliotheque.ldf',
+size=10Mb
+)
 use bibliotheque
 create table ouvrage(
-tit_ovr varchar(50) primary key,
+titre_ovr varchar(50) primary key,
 nb_exp int ,
-aut varchar(10),
-an_Edit int,
+auteur varchar(10),
+an_edt int,
+seuil int
 )
 create table livre(
 num_inv varchar(5),
 date_acq Date,
-tit_ovr varchar(50) ,
-constraint fk_livre_ouvrage foreign key(tit_ovr) references ouvrage(tit_ovr)
+titre_ovr varchar(50) ,
+constraint fk_livre_ouvrage foreign key(titre_ovr) references ouvrage(titre_ovr)
 )
 create table fournisseur(
-cod_fo varchar(5) primary key,
-tel_fo varchar(15),
-ad_fo varchar(50)
+codeF varchar(5) primary key,
+nomF varchar(20),
+telF varchar(15),
+adF varchar(50)
 )
+
 create table facteurAchat(
-num_fa_achat int primary key,
-cod_fo varchar(5) ,
-constraint fk_facteurAchat_fournisseur foreign key(cod_fo) references fournisseur(cod_fo)
+n_fa_achat int primary key,
+date_ach Date,
+codeF varchar(5) ,
+constraint fk_facteurAchat_fournisseur foreign key(codeF) references fournisseur(codeF)
 )
 create table ligneAchat(
-tit_ovr varchar(50),
-num_fa_achat int,
+titre_ovr varchar(50),
+n_fa_achat int,
 nb_ovr_ach int,
 prix_u_ach int,
-date_ach Date,
-constraint fk_ligneAchat_ouvrage_tit_ovr foreign key(tit_ovr) references ouvrage(tit_ovr),
-constraint fk_ligneAchat_ouvrage_num_fa foreign key(num_fa_achat) references facteurAchat(num_fa_achat)
+constraint fk_ligneAchat_ouvrage_titre_ovr foreign key(titre_ovr) references ouvrage(titre_ovr),
+constraint fk_ligneAchat_ouvrage_n_fa foreign key(n_fa_achat) references facteurAchat(n_fa_achat)
 )
 create table facteurVent(
-num_fa_ven int primary key,
-nom_clt varchar(15),
+n_fa_ven int primary key,
+date_ven Date,
+nom_client varchar(15),
 )
 create table ligneVente(
-num_fa_ven int,
-tit_ovr varchar(50),
-date_ven Date,
+n_fa_ven int,
+titre_ovr varchar(50),
 nb_ovr_ven int,
 prix_u_ven int
-constraint fk_ligneVente_ouvrage_tit_ovr foreign key(tit_ovr) references ouvrage(tit_ovr),
-constraint fk_lignevente_facteurVent_num_fa foreign key(num_fa_ven) references facteurVent(num_fa_ven)
+constraint fk_ligneVente_ouvrage_tit_ovr foreign key(titre_ovr) references ouvrage(titre_ovr),
+constraint fk_lignevente_facteurVent_num_fa foreign key(n_fa_ven) references facteurVent(n_fa_ven)
 )
